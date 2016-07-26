983998d172de75487d3440df2f9a09d152da9eec
akki2790 <akki2790@gmail.com>
2016-07-26 01:15:15 -0700

    Update dequeue.py

--- dequeue.py
+++ dequeue.py
@@ -10,7 +10,7 @@ class dequeue():
         self.items.append(item)
 
     def add_rear(self,item):
-        self.item.insert(0,item)
+        self.items.insert(0,item)
 
     def rem_front(self):
         return self.items.pop()
